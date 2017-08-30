2b0e6549deab7fd510625a31382de217c2d87b03
user921 <krank2014@ya.ru>
2017-08-30 14:28:11 +0300

    feature: add brain-calc; refactor;

---
+++ src/bin/games/brain-even.js
@@ -0,0 +1,16 @@
+#!/usr/bin/env node
+
+import { startGame } from '../..';
+
+console.log('Welcome to the Brain Games!');
+console.log('Answer "yes" if number even otherwise answer "no".');
+
+const numberOfRounds = 3;
+
+const generateQA = () => {
+  const random = Math.floor(Math.random() * 1000);
+  const correctAnswer = random % 2 === 0 ? 'yes' : 'no';
+  return [`${random}`, correctAnswer];
+};
+
+startGame(numberOfRounds, generateQA);
--- src/index.js
+++ src/index.js
@@ -6,11 +6,9 @@ export const askNameAndSayHello = () => {
   return name;
 };
 
-export const makeEvenRound = () => {
-  const random = Math.floor(Math.random() * 1000);
-  const correctAnswer = random % 2 === 0 ? 'yes' : 'no';
+export const makeEvenRound = (question, correctAnswer) => {
 
-  console.log(`\nQuestion: ${random}`);
+  console.log(`\nQuestion: ${question}`);
   const answer = readlineSync.question('Your answer: ').toLowerCase();
 
   if (answer !== correctAnswer) {
@@ -19,4 +17,19 @@ export const makeEvenRound = () => {
   }
   console.log('Correct!');
   return true;
+};
+
+export const startGame = (numberOfRounds, generateQA) => {
+  const name = askNameAndSayHello();
+
+  for (let i = 1; i <= numberOfRounds; i += 1) {
+    const [question, correctAnswer] = generateQA();
+    if (!makeEvenRound(question, correctAnswer)) {
+      console.log(`\nLet's try again, ${name}!`);
+      break;
+    }
+    if (i === numberOfRounds) {
+      console.log(`\nCongratulations, ${name}!`);
+    }
+  }
 };
