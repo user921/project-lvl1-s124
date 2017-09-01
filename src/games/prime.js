ea50834b78764cc8406aa8e237d98ba995cd7092
user921 <krank2014@ya.ru>
2017-09-01 08:53:01 +0300

    feature: add brain-prime

---
+++ src/games/prime.js
@@ -0,0 +1,37 @@
+import { startGame } from '..';
+import generateRandomNumber from './utils';
+
+const quest = 'Answer "yes" if number prime otherwise answer "no".';
+
+const isPrime = (num) => {
+  const iter = (i) => {
+    if (i === 1) {
+      return true;
+    }
+    if (num % i === 0) {
+      return false;
+    }
+    return iter(i - 1);
+  };
+  return iter(Math.floor(num / 2));
+};
+
+const generateQuestion = () => {
+  const question = String(generateRandomNumber(30) + 1);
+  return question;
+};
+
+const generateAnswer = (question) => {
+  const answer = isPrime(Number(question)) ? 'yes' : 'no';
+  return answer;
+};
+
+const generateQA = () => {
+  const question = generateQuestion();
+  const answer = generateAnswer(question);
+  return [question, answer];
+};
+
+export default () => {
+  startGame(quest, generateQA);
+};
