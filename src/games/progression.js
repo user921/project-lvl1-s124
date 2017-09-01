f3a5d94a14f1aa341472b819051f85018450a5a5
user921 <krank2014@ya.ru>
2017-09-01 09:40:54 +0300

    fix progression.js: get rid off magic numbers

--- src/games/progression.js
+++ src/games/progression.js
@@ -10,17 +10,20 @@ const createProgression = (first, delta, length) => {
   return [first].concat(createProgression(first + delta, delta, length - 1));
 };
 
+const progressionLength = 10; // must be bigger than 3
+const hiddenNumberIndex = Math.floor(progressionLength / 2);
+
 const generateQuestion = () => {
   const first = generateRandomNumber(100) + 1;
   const delta = generateRandomNumber(100) + 1;
-  const arr = createProgression(first, delta, 10);
-  const question = arr.map((num, i) => (i === 5 ? '..' : `${num}`)).join(' ');
+  const arr = createProgression(first, delta, progressionLength);
+  const question = arr.map((num, i) => (i === hiddenNumberIndex ? '..' : `${num}`)).join(' ');
   return question;
 };
 
 const generateAnswer = (question) => {
   const arr = question.split(' ');
-  const answer = String(arr[6] - (arr[1] - arr[0]));
+  const answer = String(arr[hiddenNumberIndex + 1] - (arr[1] - arr[0]));
   return answer;
 };
 
